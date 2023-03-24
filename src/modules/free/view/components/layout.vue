dd42b7fb79266dd1aa0d73f1bd3bfaf9fad1efd5
aslancer <x.zhiquan@gmail.com>
2023-03-24 10:39:31 +0800

    style in mobile device

--- src/modules/free/view/components/layout.vue
+++ src/modules/free/view/components/layout.vue
@@ -2,23 +2,23 @@
   <q-layout view="hHh lpR fFf">
     <q-header reveal bordered class="">
       <q-toolbar>
-        <q-toolbar-title class="cursor-pointer row items-center" @click="$router.push('/')">
-          <q-avatar class="q-mr-sm">
+        <q-toolbar-title class="site-title cursor-pointer row items-center" @click="$router.push('/')">
+          <q-avatar class="site-logo q-mr-sm">
             <img src="logo.png" style="border: 1px solid #0078DC30; border-radius: 50%;" >
           </q-avatar>
-          FreeEIS
+          <span class="site-name-text">FreeEIS</span>
         </q-toolbar-title>
         <q-space></q-space>
-        <q-btn :label="$t('Home')" flat dense to="/" class="q-mx-sm" icon="home"></q-btn>
+        <q-btn :label="$t('Home')" flat dense to="/" class="menu-home-btn q-mx-sm" icon="home"></q-btn>
         <q-btn :label="$t('Introduction')" flat dense to="/intro" class="q-mx-sm" icon="preview"></q-btn>
         <q-btn :label="$t('FE')" flat dense to="/fe" class="q-mx-sm" icon="article"></q-btn>
         <q-btn :label="$t('BE')" flat dense to="/be" class="q-mx-sm" icon="api"></q-btn>
         <q-btn :label="$t('Integration')" flat dense to="/integration" class="q-mx-sm" icon="api"></q-btn>
 
-        <select-locales class="q-mx-sm"></select-locales>
-        <theme-switch></theme-switch>
+        <select-locales class="menu-locale-btn q-mx-sm"></select-locales>
+        <theme-switch class="menu-theme-btn"></theme-switch>
 
-        <q-btn label="" flat dense @click="toGithub" class="q-mx-md"
+        <q-btn label="" flat dense @click="toGithub" class="menu-github-btn q-mx-md"
           icon="fab fa-github"></q-btn>
       </q-toolbar>
     </q-header>
@@ -70,3 +70,31 @@ export default defineComponent({
   }
 })
 </script>
+
+<style lang="scss">
+@media screen and (max-width: 980px) {
+  .site-name-text {
+    display: none;
+  }
+}
+
+@media screen and (max-width: 900px) {
+  .menu-home-btn {
+    display: none;
+  }
+}
+
+@media screen and (max-width: 800px) {
+  .q-header {
+    .q-btn .block {
+      display: none !important;
+    }
+  }
+}
+
+@media screen and (max-width: 430px) {
+  .menu-github-btn, .site-title {
+    display: none;
+  }
+}
+</style>
